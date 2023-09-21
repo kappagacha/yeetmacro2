627d24a3ca2ef17622bd1bc0b3031562c20ae22a
adiamante <angelo.c.diamante@gmail.com>
2023-09-21 17:33:26 -0500

    debug Eversoul doArena

--- YeetMacro2/Services/Scripts/Eversoul/doArena.cs
+++ YeetMacro2/Services/Scripts/Eversoul/doArena.cs
@@ -24,45 +24,45 @@ public partial class EversoulScripts
                     Sleep(500);
                     macroService.PollPattern(patterns["adventure"]["arena"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["adventure"]["arena"]["info"] });
                     break;
-                case "adventure.arena.freeChallenge":
-                    logger.LogInformation("doArena: free challenges");
-                    macroService.PollPattern(patterns["adventure"]["arena"]["freeChallenge"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["adventure"]["arena"]["startMatch"] });
-                    break;
-                case "adventure.arena.startMatch":
-                    logger.LogInformation("doArena: start match");
-                    var match1CP = Regex.Replace(macroService.GetText(patterns["adventure"]["arena"]["match1"]["cp"]), "[, ]", "");
-                    var match2CP = Regex.Replace(macroService.GetText(patterns["adventure"]["arena"]["match2"]["cp"]), "[, ]", "");
-                    var match3CP = Regex.Replace(macroService.GetText(patterns["adventure"]["arena"]["match3"]["cp"]), "[, ]", "");
+                //case "adventure.arena.freeChallenge":
+                //    logger.LogInformation("doArena: free challenges");
+                //    macroService.PollPattern(patterns["adventure"]["arena"]["freeChallenge"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["adventure"]["arena"]["startMatch"] });
+                //    break;
+                //case "adventure.arena.startMatch":
+                //    logger.LogInformation("doArena: start match");
+                //    var match1CP = Regex.Replace(macroService.GetText(patterns["adventure"]["arena"]["match1"]["cp"]), "[, ]", "");
+                //    var match2CP = Regex.Replace(macroService.GetText(patterns["adventure"]["arena"]["match2"]["cp"]), "[, ]", "");
+                //    var match3CP = Regex.Replace(macroService.GetText(patterns["adventure"]["arena"]["match3"]["cp"]), "[, ]", "");
 
-                    logger.LogDebug("match1CP: " + match1CP);
-                    logger.LogDebug("match2CP: " + match2CP);
-                    logger.LogDebug("match3CP: " + match3CP);
+                //    logger.LogDebug("match1CP: " + match1CP);
+                //    logger.LogDebug("match2CP: " + match2CP);
+                //    logger.LogDebug("match3CP: " + match3CP);
 
-                    var matches = new int[] { int.Parse(match1CP), int.Parse(match2CP), int.Parse(match3CP) };
-                    int minIdx = matches
-                        .Select((val, idx) => new { Value = val, Index = idx })
-                        .Aggregate((min, current) => current.Value < min.Value ? current : min)
-                        .Index;
-                    var minCP = matches[minIdx];
-                    var cpThreshold = int.Parse(settings["arena"]["cpThreshold"].GetValue<string>());
+                //    var matches = new int[] { int.Parse(match1CP), int.Parse(match2CP), int.Parse(match3CP) };
+                //    int minIdx = matches
+                //        .Select((val, idx) => new { Value = val, Index = idx })
+                //        .Aggregate((min, current) => current.Value < min.Value ? current : min)
+                //        .Index;
+                //    var minCP = matches[minIdx];
+                //    var cpThreshold = int.Parse(settings["arena"]["cpThreshold"].GetValue<string>());
 
-                    logger.LogDebug("minIdx: " + minIdx);
-                    logger.LogDebug("minCP: " + minCP);
-                    logger.LogDebug("cpThreshold: " + cpThreshold);
-                    if (minCP <= cpThreshold)
-                    {
-                        macroService.PollPattern(patterns["adventure"]["arena"]["match" + (minIdx + 1)]["challenge"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["battle"]["start"] });
-                        Sleep(500);
-                        macroService.PollPattern(patterns["battle"]["skip"]["disabled"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["battle"]["skip"]["enabled"] });
-                        macroService.PollPattern(patterns["battle"]["start"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["prompt"]["confirm2"] });
-                        macroService.PollPattern(patterns["prompt"]["confirm2"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = new PatternNode[] { patterns["adventure"]["arena"]["freeChallenge"], patterns["adventure"]["arena"]["ticket"] } });
-                        //macroService.PollPattern(patterns["battle"]["start"], new PollPatternFindOptions() { DoClick = true, ClickPattern = patterns["battle"]["skip"], PredicatePattern = new PatternNode[] { patterns["adventure"]["arena"]["freeChallenge"], patterns["adventure"]["arena"]["ticket"] } });
-                    }
-                    else
-                    {
-                        macroService.PollPattern(patterns["battle"]["rematch"], new PollPatternFindOptions() { DoClick = true, InversePredicatePattern = patterns["battle"]["rematch"] });
-                    }
-                    break;
+                //    logger.LogDebug("minIdx: " + minIdx);
+                //    logger.LogDebug("minCP: " + minCP);
+                //    logger.LogDebug("cpThreshold: " + cpThreshold);
+                //    if (minCP <= cpThreshold)
+                //    {
+                //        macroService.PollPattern(patterns["adventure"]["arena"]["match" + (minIdx + 1)]["challenge"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["battle"]["start"] });
+                //        Sleep(500);
+                //        macroService.PollPattern(patterns["battle"]["skip"]["disabled"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["battle"]["skip"]["enabled"] });
+                //        macroService.PollPattern(patterns["battle"]["start"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = patterns["prompt"]["confirm2"] });
+                //        macroService.PollPattern(patterns["prompt"]["confirm2"], new PollPatternFindOptions() { DoClick = true, PredicatePattern = new PatternNode[] { patterns["adventure"]["arena"]["freeChallenge"], patterns["adventure"]["arena"]["ticket"] } });
+                //        //macroService.PollPattern(patterns["battle"]["start"], new PollPatternFindOptions() { DoClick = true, ClickPattern = patterns["battle"]["skip"], PredicatePattern = new PatternNode[] { patterns["adventure"]["arena"]["freeChallenge"], patterns["adventure"]["arena"]["ticket"] } });
+                //    }
+                //    else
+                //    {
+                //        macroService.PollPattern(patterns["battle"]["rematch"], new PollPatternFindOptions() { DoClick = true, InversePredicatePattern = patterns["battle"]["rematch"] });
+                //    }
+                //    break;
                 case "adventure.arena.ticket":
                     //logger.LogInformation("doArena: done");
                     //return String.Empty;
