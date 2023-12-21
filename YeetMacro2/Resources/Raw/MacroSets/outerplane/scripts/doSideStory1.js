a3e1e03eb2701279609a54e444ffb2b4fed2a679
adiamante <angelo.c.diamante@gmail.com>
2023-12-20 21:00:09 -0600

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doSideStory1.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doSideStory1.js
@@ -11,6 +11,7 @@ if (daily[`doSideStory${sideStoryNumber}`].done.IsChecked) {
 
 while (macroService.IsRunning) {
 	const loopResult = macroService.PollPattern(loopPatterns);
+	let sideStorySection;
 	switch (loopResult.Path) {
 		case 'lobby.level':
 			logger.info(`doSideStory${sideStoryNumber}: click adventure tab`);
@@ -27,28 +28,28 @@ while (macroService.IsRunning) {
 			switch (targetCharacter) {
 				case 'noa':
 				case 'veronica':
-					macroService.PollPattern(patterns.sideStory.theOtherSideOfTheWorld, { DoClick: true, PredicatePattern: patterns.sideStory.theOtherSideOfTheWorld.selected });
-					const swipeResult = macroService.SwipePollPattern(patterns.sideStory.theOtherSideOfTheWorld[targetCharacter], { Start: { X: 1000, Y: 850 }, End: { X: 500, Y: 850 } });
-					if (!swipeResult.IsSuccess) {
-						throw new Error(`Could not find side story for ${targetCharacter}`);
-					}
-					const targetCharacterResult = macroService.PollPattern(patterns.sideStory.theOtherSideOfTheWorld[targetCharacter], { DoClick: true, PredicatePattern: [patterns.battle.selectTeam, patterns.sideStory.enter] });
-					if (targetCharacterResult.PredicatePath === 'sideStory.enter') {
-						const threeStarsResult = macroService.FindPattern(patterns.sideStory.threeStars, { Limit: 10 });
-						const maxY = threeStarsResult.Points.reduce((maxY, p) => (maxY = maxY > p.Y ? maxY : p.Y), 0);
-						if (!maxY) {
-							throw new Error('Could not find bottom three stars');
-						}
-						const bottomThreeStars = macroService.ClonePattern(patterns.sideStory.threeStars, { CenterY: maxY, Height: 60, Path: `sideStory.threeStars_${maxY}` });
-						macroService.PollPattern(bottomThreeStars, { DoClick: true, ClickOffset: { X: -100 }, PredicatePattern: patterns.battle.selectTeam });
-					}
-					macroService.PollPattern(patterns.battle.selectTeam, { DoClick: true, PredicatePattern: patterns.battle.setup.auto });
-					selectTeamAndBattle(teamSlot, sweepBattle);
-					if (macroService.IsRunning) {
-						daily[`doSideStory${sideStoryNumber}`].done.IsChecked = true;
-					}
+					sideStorySection = patterns.sideStory.theOtherSideOfTheWorld;
 					break;
+				case 'valentine':
+					sideStorySection = patterns.sideStory.aBriefInterlude;
+					break;
+				default:
+					throw new Error(`Unhandled targetCharacter: ${targetCharacter}`);
 			}
+
+			macroService.PollPattern(sideStorySection, { DoClick: true, PredicatePattern: sideStorySection.selected });
+			const swipeResult = macroService.SwipePollPattern(sideStorySection[targetCharacter], { Start: { X: 1000, Y: 850 }, End: { X: 500, Y: 850 } });
+			if (!swipeResult.IsSuccess) {
+				throw new Error(`Could not find side story for ${targetCharacter}`);
+			}
+			macroService.PollPattern(sideStorySection[targetCharacter], { DoClick: true, PredicatePattern: [patterns.battle.selectTeam, patterns.sideStory.enter] });
+			macroService.PollPattern(patterns.sideStory.shardStage, { DoClick: true, PredicatePattern: patterns.sideStory.rewardSetting });
+			macroService.PollPattern(patterns.battle.selectTeam, { DoClick: true, PredicatePattern: patterns.battle.setup.auto });
+			selectTeamAndBattle(teamSlot, sweepBattle);
+			if (macroService.IsRunning) {
+				daily[`doSideStory${sideStoryNumber}`].done.IsChecked = true;
+			}
+
 			return;
 	}
 	sleep(1_000);
