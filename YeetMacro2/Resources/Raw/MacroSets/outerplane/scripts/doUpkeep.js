fa52aaeeb8fb73505ba79571888a8f25c5acc6c1
adiamante <angelo.c.diamante@gmail.com>
2023-12-28 21:15:12 -0600

    update outerplane

---
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doUpkeep.js
@@ -0,0 +1,21 @@
+// Do dailies (all checked scripts)
+
+if (settings.doUpkeep.claimAntiparticle.Value) {
+    claimAntiparticle();
+    goToLobby();
+}
+
+if (settings.doUpkeep.doArena.Value) {
+    doArena();
+    goToLobby();
+}
+
+if (settings.doUpkeep.doEcologyStudy.Value) {
+    doEcologyStudy();
+    goToLobby();
+}
+
+if (settings.doUpkeep.doIdentification.Value) {
+    doBanditChase();
+    goToLobby();
+}
