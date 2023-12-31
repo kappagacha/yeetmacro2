22c486d4aa4a4490909ff4acd0188cdcc1e95e4c
adiamante <angelo.c.diamante@gmail.com>
2023-12-31 15:29:36 -0600

    update StatusPanelView UI

--- YeetMacro2/ViewModels/StatusPanelViewModel.cs
+++ YeetMacro2/ViewModels/StatusPanelViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using YeetMacro2.Services;
 
 namespace YeetMacro2.ViewModels;
@@ -7,27 +6,53 @@ namespace YeetMacro2.ViewModels;
 public partial class StatusPanelViewModel : ObservableObject
 {
     IRecorderService _recorderService;
-    [ObservableProperty]
-    bool _isRecording;
+    IToastService _toastService;
     [ObservableProperty]
     LogViewModel _logViewModel;
-    public StatusPanelViewModel(IRecorderService recorderService, LogViewModel logViewModel)
+    [ObservableProperty]
+    bool _isRecording;
+    public StatusPanelViewModel(IRecorderService recorderService, IToastService toastService, LogViewModel logViewModel)
     {
         _recorderService = recorderService;
+        _toastService = toastService;
         _logViewModel = logViewModel;
     }
 
-
-    [RelayCommand]
-    public void ToggleRecording()
+    async partial void OnIsRecordingChanged(bool value)
     {
-        if (!IsRecording)
+        if (await Permissions.RequestAsync<Permissions.StorageWrite>() != PermissionStatus.Granted) return;
+
+#if ANDROID
+        // https://stackoverflow.com/questions/75880663/maui-on-android-listing-folder-contents-of-an-sd-card-and-writing-in-it
+        if (global::Android.OS.Build.VERSION.SdkInt >= global::Android.OS.BuildVersionCodes.R && !Android.OS.Environment.IsExternalStorageManager)
         {
-            _recorderService.StopRecording();
+            var intent = new Android.Content.Intent();
+            intent.SetAction(Android.Provider.Settings.ActionManageAppAllFilesAccessPermission);
+            Android.Net.Uri uri = Android.Net.Uri.FromParts("package", Platform.CurrentActivity.PackageName, null);
+            intent.SetData(uri);
+            Platform.CurrentActivity.StartActivity(intent);
+            _isRecording = false;
+            OnPropertyChanged(nameof(IsRecording));
+            return;
         }
-        else
+#endif
+
+        try
         {
-            _recorderService.StartRecording();
+            if (value)
+            {
+                _recorderService.StartRecording();
+                _toastService.Show("Started Recording...");
+            }
+            else
+            {
+                _recorderService.StopRecording();
+                _toastService.Show("Stopped Recording...");
+            }
+        }
+        catch (Exception ex)
+        {
+            _toastService.Show($"Toggle IsRecording Failed: ${ex.Message}");
         }
     }
 }
