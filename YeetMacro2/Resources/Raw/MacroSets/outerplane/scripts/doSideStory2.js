6cfcaf142ae8fd7bb99a7f44934b58cb260b8ec7
adiamante <angelo.c.diamante@gmail.com>
2024-01-03 20:38:06 -0600

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doSideStory2.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doSideStory2.js
@@ -1,17 +1,28 @@
-// Auto or sweep side story (2)
+// Auto or sweep side story (2). Required options:
+// section: selects the tab
+// targetStory: pattern the selects story
 const loopPatterns = [patterns.lobby.level, patterns.titles.adventure, patterns.titles.sideStory];
 const daily = dailyManager.GetDaily();
 const sideStoryNumber = 2;
-const targetCharacter = settings[`doSideStory${sideStoryNumber}`].targetCharacter.Value;
 const sweepBattle = settings[`doSideStory${sideStoryNumber}`].sweepBattle.Value;
 const teamSlot = settings[`doSideStory${sideStoryNumber}`].teamSlot.Value;
+const sideStorySection = patterns.sideStory[settings[`doSideStory${sideStoryNumber}`].section.Value];
+const resolution = macroService.GetCurrentResolution();
+const targetStory = macroService.ClonePattern(settings[`doSideStory${sideStoryNumber}`].targetStory.Value, {
+	X: 450,
+	Y: 700,
+	Width: resolution.Width - 500,
+	Height: 300,
+	Path: 'settings.outings.target',
+	OffsetCalcType: 'DockLeft'
+});
+
 if (daily[`doSideStory${sideStoryNumber}`].done.IsChecked) {
 	return "Script already completed. Uncheck done to override daily flag.";
 }
 
 while (macroService.IsRunning) {
 	const loopResult = macroService.PollPattern(loopPatterns);
-	let sideStorySection;
 	switch (loopResult.Path) {
 		case 'lobby.level':
 			logger.info(`doSideStory${sideStoryNumber}: click adventure tab`);
@@ -24,26 +35,14 @@ while (macroService.IsRunning) {
 			sleep(500);
 			break;
 		case 'titles.sideStory':
-			logger.info(`doSideStory${sideStoryNumber}: ${targetCharacter}`);
-			switch (targetCharacter) {
-				case 'noa':
-				case 'veronica':
-				case 'ame':
-					sideStorySection = patterns.sideStory.theOtherSideOfTheWorld;
-					break;
-				case 'valentine':
-					sideStorySection = patterns.sideStory.aBriefInterlude;
-					break;
-				default:
-					throw new Error(`Unhandled targetCharacter: ${targetCharacter}`);
-			}
+			logger.info(`doSideStory${sideStoryNumber}: auto or sweep`);
 
 			macroService.PollPattern(sideStorySection, { DoClick: true, PredicatePattern: sideStorySection.selected });
-			const swipeResult = macroService.SwipePollPattern(sideStorySection[targetCharacter], { Start: { X: 1000, Y: 850 }, End: { X: 500, Y: 850 } });
+			const swipeResult = macroService.SwipePollPattern(targetStory, { Start: { X: 1000, Y: 850 }, End: { X: 500, Y: 850 } });
 			if (!swipeResult.IsSuccess) {
-				throw new Error(`Could not find side story for ${targetCharacter}`);
+				throw new Error('Could not find target side story');
 			}
-			macroService.PollPattern(sideStorySection[targetCharacter], { DoClick: true, PredicatePattern: [patterns.battle.selectTeam, patterns.sideStory.enter] });
+			macroService.PollPattern(targetStory, { DoClick: true, PredicatePattern: [patterns.battle.selectTeam, patterns.sideStory.enter] });
 			macroService.PollPattern(patterns.sideStory.shardStage, { DoClick: true, PredicatePattern: patterns.sideStory.rewardSetting });
 			macroService.PollPattern(patterns.battle.selectTeam, { DoClick: true, PredicatePattern: patterns.battle.setup.auto });
 			selectTeamAndBattle(teamSlot, sweepBattle);
