248181c116ec08e6ea506e528aab9da196fecb4d
adiamante <angelo.c.diamante@gmail.com>
2024-01-18 18:13:19 -0600

    update eversoul

--- YeetMacro2/Resources/Raw/MacroSets/eversoul/scripts/claimFreeSummon.js
+++ YeetMacro2/Resources/Raw/MacroSets/eversoul/scripts/claimFreeSummon.js
@@ -31,6 +31,8 @@ while (macroService.IsRunning) {
 			macroService.PollPattern(patterns.summon.skip, { DoClick: true, PredicatePattern: patterns.general.back, IntervalDelayMs: 2_500 });
 
 			if (settings.claimFreeSummon.doOneArtifact.Value && !daily.claimFreeSummon.doOneArtifact.IsChecked) {
+				macroService.PollPattern(patterns.general.back, { DoClick: true, PredicatePattern: patterns.summon.info });
+
 				const artifactSwipeResult = macroService.SwipePollPattern(patterns.summon.artifact, { Start: { X: 100, Y: 650 }, End: { X: 100, Y: 200 } });
 				if (!artifactSwipeResult.IsSuccess) {
 					throw new Error('Unable to find artifact summon');
