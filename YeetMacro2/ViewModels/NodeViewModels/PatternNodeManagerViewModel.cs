ef028282e6f2267dbf0b582941fa11b546221a0f
adiamante <angelo.c.diamante@gmail.com>
2024-01-19 22:46:35 -0600

    update outerplane

--- YeetMacro2/ViewModels/NodeViewModels/PatternNodeManagerViewModel.cs
+++ YeetMacro2/ViewModels/NodeViewModels/PatternNodeManagerViewModel.cs
@@ -219,7 +219,7 @@ public partial class PatternNodeManagerViewModel : NodeManagerViewModel<PatternN
             case OffsetCalcType.Center:
                 {   // horizontal center handling
                     var deltaX = currentResolution.Width - pattern.Resolution.Width + topLeft.X;
-                    xOffset = (int)(deltaX / 2); // + (deltaX % 2 == 0 ? 0 : 1);
+                    xOffset = (int)(deltaX / 2);
                 }
                 break;
             case OffsetCalcType.DockRight:
@@ -233,14 +233,14 @@ public partial class PatternNodeManagerViewModel : NodeManagerViewModel<PatternN
                 {
                     // HorizontalStretchMultiplier = targetXOffset / deltaX
                     var deltaX = currentResolution.Width - pattern.Resolution.Width - topLeft.X;
-                    xOffset = (int)(deltaX * pattern.HorizontalStretchMultiplier) + (int)topLeft.X; // + (deltaX % 2 == 0 ? 0 : 1);
+                    xOffset = (int)(deltaX * pattern.HorizontalStretchMultiplier) + (int)topLeft.X;
                 }
                 break;
             case OffsetCalcType.VerticalStretchOffset:
                 {
                     // HorizontalStretchMultiplier = targetYOffset / deltaY
                     var deltaY = currentResolution.Height - pattern.Resolution.Height - topLeft.Y;
-                    yOffset = (int)(deltaY * pattern.VerticalStretchMultiplier) + (int)topLeft.Y; // + (deltaY % 2 == 0 ? 0 : 1);
+                    yOffset = (int)(deltaY * pattern.VerticalStretchMultiplier) + (int)topLeft.Y;
                 }
                 break;
         }
