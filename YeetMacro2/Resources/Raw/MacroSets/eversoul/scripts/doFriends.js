43c5ab7df1a8d59a403292a3ccc6119c882b8bed
adiamante <angelo.c.diamante@gmail.com>
2024-02-27 19:14:32 -0600

    update eversoul

--- YeetMacro2/Resources/Raw/MacroSets/eversoul/scripts/doFriends.js
+++ YeetMacro2/Resources/Raw/MacroSets/eversoul/scripts/doFriends.js
@@ -45,8 +45,8 @@ while (macroService.IsRunning) {
 				//let hireSoulDoneResult = { IsSuccess: false };
 				let hireTargets = [hireTarget1, hireTarget2, hireTarget3, hireTarget4, hireTarget5];
 
-				while (macroService.IsRunning && !hireSoulDoneResult.IsSuccess && swipeCount < 15) {
-					const swipeResult = macroService.SwipePollPattern(hireTargets, { MaxSwipes: 1, Start: { X: 1500, Y: 800 }, End: { X: 1500, Y: 150 } });
+				while (macroService.IsRunning && !hireSoulDoneResult.IsSuccess && swipeCount < 20) {
+					const swipeResult = macroService.SwipePollPattern(hireTargets, { MaxSwipes: 1, Start: { X: 1500, Y: 700 }, End: { X: 1500, Y: 150 } });
 					if (swipeResult.IsSuccess) {
 						const hirePattern = macroService.ClonePattern(patterns.friends.hireSoul.hire, { CenterY: swipeResult.Point.Y, Padding: 25, Path: `friends.hireSoul.hire_y${swipeResult.Point.Y}` });
 						const hirePatternResult = macroService.FindPattern(hirePattern);
