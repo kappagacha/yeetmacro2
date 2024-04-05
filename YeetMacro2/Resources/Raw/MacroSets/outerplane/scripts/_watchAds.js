239c08bca72665cd07181dc41ba28793463e731c
adiamante <angelo.c.diamante@gmail.com>
2024-04-05 18:03:22 -0500

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/_watchAds.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/_watchAds.js
@@ -2,7 +2,7 @@
 // @position=-100
 // Watches all ads in a loop
 
-const loopPatterns = [patterns.lobby.level, patterns.stamina.purchase, userClickPattern];
+const loopPatterns = [patterns.lobby.level, patterns.stamina.purchase];
 //const daily = dailyManager.GetDaily();
 //if (daily.watchAds.count.Count >= 15) {
 //	return;
