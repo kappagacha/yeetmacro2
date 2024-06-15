dea59d471be859c0336f9a1ba063d863f7b93880
adiamante <angelo.c.diamante@gmail.com>
2024-06-14 22:58:31 -0500

    update SettingsNodeView

--- YeetMacro2/Views/DataTemplateView.cs
+++ YeetMacro2/Views/DataTemplateView.cs
@@ -5,11 +5,19 @@ public class DataTemplateView : ContentView
 {
     public static readonly BindableProperty DataTemplateProperty =
         BindableProperty.Create("DataTemplate", typeof(DataTemplate), typeof(DataTemplateView), null, propertyChanged: DataTemplate_Changed);
+    public static readonly BindableProperty ContentTemplateProperty =
+        BindableProperty.Create("ContentTemplate", typeof(DataTemplate), typeof(DataTemplateView), null, propertyChanged: ContentTemplate_Changed);
 
     public DataTemplate DataTemplate
     {
         get { return (DataTemplate)GetValue(DataTemplateProperty); }
         set { SetValue(DataTemplateProperty, value); }
+    }
+
+    public DataTemplate ContentTemplate
+    {
+        get { return (DataTemplate)GetValue(ContentTemplateProperty); }
+        set { SetValue(ContentTemplateProperty, value); }
     }
 
     private static void DataTemplate_Changed(BindableObject bindable, object oldValue, object newValue)
@@ -18,11 +26,37 @@ public class DataTemplateView : ContentView
         {
             var dataTemplateView = bindable as DataTemplateView;
             dataTemplateView.Content = (View)dataTemplate.CreateContent();
+
+            if (dataTemplateView.ContentTemplate is null) return;
+            var contentPresenter = (ContentPresenter)dataTemplateView.Content.FindByName("contentPresenter");
+            if (contentPresenter is null) return;
+            contentPresenter.Content = (View)dataTemplateView.ContentTemplate.CreateContent();
+        }
+    }
+
+    private static void ContentTemplate_Changed(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (newValue is DataTemplate dataTemplate)
+        {
+            var dataTemplateView = bindable as DataTemplateView;
+            var contentPresenter = (ContentPresenter)dataTemplateView.Content.FindByName("contentPresenter");
+            if (contentPresenter is null) return;
+            contentPresenter.Content = (View)dataTemplate.CreateContent();
+            contentPresenter.Content.BindingContext = dataTemplateView.BindingContext;
         }
     }
 
     public DataTemplateView()
 	{
-
+        BindingContextChanged += DataTemplateView_BindingContextChanged;
 	}
+
+    private void DataTemplateView_BindingContextChanged(object sender, EventArgs e)
+    {
+        if (ContentTemplate is null) return;
+
+        var contentPresenter = (ContentPresenter)Content.FindByName("contentPresenter");
+        if (contentPresenter is null) return;
+        contentPresenter.Content.BindingContext = BindingContext;
+    }
 }
