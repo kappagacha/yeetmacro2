6f476d33c9b3ab9fe4361d252d2037b2b712e422
adiamante <angelo.c.diamante@gmail.com>
2024-06-20 19:42:50 -0500

    update eversoul

--- YeetMacro2/Resources/Raw/MacroSets/eversoul/scripts/sweepEvent.js
+++ YeetMacro2/Resources/Raw/MacroSets/eversoul/scripts/sweepEvent.js
@@ -42,27 +42,60 @@ while (macroService.IsRunning) {
 			macroService.PollPattern(patterns.lobby.attendanceCheck, { DoClick: true, PredicatePattern: patterns.general.back });
 			break;
 		case 'general.back':
-			const eventRaidSwipeResult = macroService.SwipePollPattern(eventRaidPattern, { Start: { X: 50, Y: 800 }, End: { X: 50, Y: 500 } });
-			if (!eventRaidSwipeResult.IsSuccess) {
-				throw Error('Unable to find pattern: settings.eventRaid.eventRaidPattern');
+			if (!daily.sweepEvent.eventRaid.IsChecked) {
+				const eventRaidSwipeResult = macroService.SwipePollPattern(eventRaidPattern, { Start: { X: 50, Y: 800 }, End: { X: 50, Y: 500 } });
+				if (!eventRaidSwipeResult.IsSuccess) {
+					throw Error('Unable to find pattern: settings.eventRaid.eventRaidPattern');
+				}
+				macroService.PollPattern(eventRaidPattern, { DoClick: true, PredicatePattern: patterns.event.eventRaid });
+				macroService.PollPattern(patterns.event.eventRaid, { DoClick: true, PredicatePattern: patterns.event.eventRaid.sweep, ClickOffset: { Y: 60 } });
+				macroService.PollPattern(patterns.event.eventRaid.sweep, { DoClick: true, PredicatePattern: patterns.event.eventRaid.sweep.sweep });
+				macroService.PollPattern(patterns.event.max, { DoClick: true, InversePredicatePattern: patterns.event.max });
+				macroService.PollPattern(patterns.event.eventRaid.sweep.sweep, { DoClick: true, PredicatePattern: patterns.general.tapTheScreen });
+				macroService.PollPattern(patterns.general.tapTheScreen, { DoClick: true, PredicatePattern: patterns.general.back });
+				macroService.PollPattern(patterns.general.back, { DoClick: true, PredicatePattern: patterns.event.eventRaid });
+
+				if (macroService.IsRunning) {
+					daily.sweepEvent.eventRaid.IsChecked = true;
+				}
+
+				const eventStageSwipeResult = macroService.SwipePollPattern(eventStagePattern, { Start: { X: 50, Y: 800 }, End: { X: 50, Y: 500 } });
+				if (!eventStageSwipeResult.IsSuccess) {
+					throw Error('Unable to find pattern: settings.eventRaid.eventStagePattern');
+				}
 			}
-			macroService.PollPattern(eventRaidPattern, { DoClick: true, PredicatePattern: patterns.event.eventRaid });
-			macroService.PollPattern(patterns.event.eventRaid, { DoClick: true, PredicatePattern: patterns.event.eventRaid.sweep, ClickOffset: { Y: 60 } });
-			macroService.PollPattern(patterns.event.eventRaid.sweep, { DoClick: true, PredicatePattern: patterns.event.eventRaid.sweep.sweep });
-			macroService.PollPattern(patterns.event.eventRaid.max, { DoClick: true, InversePredicatePattern: patterns.event.eventRaid.max });
-			macroService.PollPattern(patterns.event.eventRaid.sweep.sweep, { DoClick: true, PredicatePattern: patterns.general.tapTheScreen });
-			macroService.PollPattern(patterns.general.tapTheScreen, { DoClick: true, PredicatePattern: patterns.general.back });
-			macroService.PollPattern(patterns.general.back, { DoClick: true, PredicatePattern: patterns.event.eventRaid });
+
+			if (!daily.sweepEvent.eventStage.IsChecked) {
+				macroService.PollPattern(eventStagePattern, { DoClick: true, PredicatePattern: patterns.event.eventStage });
+
+				// currency amount 1 is to the left in event banner page
+				const currencyAmount1 = macroService.GetText(patterns.event.eventStage.currency1.amount);
+				// currency amount 2 is to the right in event banner page
+				const currencyAmount2 = macroService.GetText(patterns.event.eventStage.currency2.amount);
+
+				logger.info(`sweepEvent: currencyAmount1 ${currencyAmount1} VS currencyAmount2 ${currencyAmount2}`);
+
+				macroService.PollPattern(patterns.event.eventStage, { DoClick: true, PredicatePattern: patterns.event.eventStage.sweep, ClickOffset: { Y: 60 } });
+
+				// currency amount 1 is the right stage
+				// currency amount 2 is the left stage
+				if (currencyAmount1 < currencyAmount2) {
+					macroService.PollPattern(patterns.event.eventStage.currency1.play, { DoClick: true, PredicatePattern: patterns.event.eventStage.currency1.enemyLvl });
+				}
+				macroService.PollPattern(patterns.event.eventStage.sweep, { DoClick: true, PredicatePattern: patterns.event.eventStage.sweep.sweep });
+				macroService.PollPattern(patterns.event.max, { DoClick: true, InversePredicatePattern: patterns.event.max });
+				macroService.PollPattern(patterns.event.eventStage.sweep.sweep, { DoClick: true, PredicatePattern: patterns.general.tapTheScreen });
+				macroService.PollPattern(patterns.general.tapTheScreen, { DoClick: true, PredicatePattern: patterns.general.back });
+				macroService.PollPattern(patterns.general.back, { DoClick: true, PredicatePattern: patterns.event.eventStage });
+
+				if (macroService.IsRunning) {
+					daily.sweepEvent.eventStage.IsChecked = true;
+				}
+			}
 
 			if (macroService.IsRunning) {
-				daily.sweepEvent.eventRaid.IsChecked = true;
+				daily.sweepEvent.done.IsChecked = true;
 			}
-
-			const eventStageSwipeResult = macroService.SwipePollPattern(eventStagePattern, { Start: { X: 50, Y: 800 }, End: { X: 50, Y: 500 } });
-			if (!eventStageSwipeResult.IsSuccess) {
-				throw Error('Unable to find pattern: settings.eventRaid.eventStagePattern');
-			}
-
 			return;
 	}
 
