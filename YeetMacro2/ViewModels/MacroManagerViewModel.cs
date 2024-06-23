ed3c337cf7b677b564eed445d23519a663a635ff
adiamante <angelo.c.diamante@gmail.com>
2024-06-22 22:39:36 -0500

    fix exporting

--- YeetMacro2/ViewModels/MacroManagerViewModel.cs
+++ YeetMacro2/ViewModels/MacroManagerViewModel.cs
@@ -357,8 +357,6 @@ public partial class MacroManagerViewModel : ObservableObject
     [RelayCommand]
     private async Task ExportMacroSet(MacroSet macroSet)
     {
-        //if (await Permissions.RequestAsync<Permissions.StorageWrite>() != PermissionStatus.Granted) return;
-
 #if ANDROID
         // https://stackoverflow.com/questions/75880663/maui-on-android-listing-folder-contents-of-an-sd-card-and-writing-in-it
         if (OperatingSystem.IsAndroidVersionAtLeast(30) && !Android.OS.Environment.IsExternalStorageManager)
@@ -368,6 +366,11 @@ public partial class MacroManagerViewModel : ObservableObject
             Android.Net.Uri uri = Android.Net.Uri.FromParts("package", Platform.CurrentActivity.PackageName, null);
             intent.SetData(uri);
             Platform.CurrentActivity.StartActivity(intent);
+            return;
+        }
+        else if (!OperatingSystem.IsAndroidVersionAtLeast(30) && await Permissions.RequestAsync<Permissions.StorageWrite>() != PermissionStatus.Granted)
+        {
+            return;
         }
 #endif
 
--- YeetMacro2/ViewModels/NodeViewModels/NodeManagerViewModel.cs
+++ YeetMacro2/ViewModels/NodeViewModels/NodeManagerViewModel.cs
@@ -412,8 +412,6 @@ public partial class NodeManagerViewModel<TViewModel, TParent, TChild> : NodeMan
     [RelayCommand]
     public async Task Export(string name)
     {
-        //if (await Permissions.RequestAsync<Permissions.StorageWrite>() != PermissionStatus.Granted) return;
-
 #if ANDROID
         // https://stackoverflow.com/questions/75880663/maui-on-android-listing-folder-contents-of-an-sd-card-and-writing-in-it
         if (OperatingSystem.IsAndroidVersionAtLeast(30) && !Android.OS.Environment.IsExternalStorageManager)
@@ -423,6 +421,11 @@ public partial class NodeManagerViewModel<TViewModel, TParent, TChild> : NodeMan
             Android.Net.Uri uri = Android.Net.Uri.FromParts("package", Platform.CurrentActivity.PackageName, null);
             intent.SetData(uri);
             Platform.CurrentActivity.StartActivity(intent);
+            return;
+        }
+        else if (!OperatingSystem.IsAndroidVersionAtLeast(30) && await Permissions.RequestAsync<Permissions.StorageWrite>() != PermissionStatus.Granted)
+        {
+            return;
         }
 #endif
 
