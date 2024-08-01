a1f1b57a6a17692be79d795b4d90943c297fe05b
adiamante <angelo.c.diamante@gmail.com>
2024-07-31 23:00:10 -0500

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/shared.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/shared.js
@@ -8,7 +8,7 @@ function selectTeam(targetTeamSlot, returnCurrentCp) {
 	let currentTeamSlot = getCurrentTeamSlot();
 	while (macroService.IsRunning && currentTeamSlot?.trim() != targetTeamSlot) {
 		const teamSlotResult = findTeamSlot(targetTeamSlot);
-		logger.info(`${teamSlotResult} => ${targetTeamSlot}`);
+		logger.info(`${currentTeamSlot} => ${targetTeamSlot}`);
 		if (teamSlotResult) {
 			macroService.DoClick(teamSlotResult);
 			sleep(2_500);
