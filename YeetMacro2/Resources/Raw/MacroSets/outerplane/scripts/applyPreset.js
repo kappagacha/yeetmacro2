54a0c1bf7e39f51c5a2e1b3d1456690b7ee2ec41
adiamante <angelo.c.diamante@gmail.com>
2024-08-17 21:46:36 -0500

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/applyPreset.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/applyPreset.js
@@ -1,6 +1,25 @@
 // @raw-script
 // Apply equipmentPreset
 
-function applyPreset(teamSlot = 1) {
-	
+function applyPreset(teamSlot) {
+	if (!applyPreset) {
+
+	}
+
+	const teamFormationLocations = ['left', 'top', 'right', 'bottom'];
+	for (const teamFormationLocation of teamFormationLocations) {
+		macroService.PollPattern(patterns.battle.teamFormation[teamFormationLocation], { DoClick: true, HoldDurationMs: 750, PredicatePattern: patterns.battle.teamFormation.preset });
+		macroService.PollPattern(patterns.battle.teamFormation.preset, { DoClick: true, PredicatePattern: patterns.battle.teamFormation.preset.presetList });
+		const presetCornerResult = macroService.FindPattern(patterns.battle.teamFormation.preset.corner, { Limit: 10 });
+		const presetNames = presetCornerResult.Points.filter(p => p).map(p => {
+			const presetNamePattern = macroService.ClonePattern(patterns.battle.teamFormation.preset.name, { X: p.X, Y: p.Y, OffsetCalcType: 'None' });
+			return {
+				point: { X: p.X, Y: p.Y },
+				name: macroService.GetText(presetNamePattern)
+			};
+		});
+	}
+
+
+	return presetNames;
 }
