0e860ccc877c1b4a218a925d5f00ae4628c83f9b
adiamante <angelo.c.diamante@gmail.com>
2024-08-18 21:27:10 -0500

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/applyPreset.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/applyPreset.js
@@ -25,7 +25,7 @@ function applyPreset(teamSlot) {
 		macroService.PollPattern(patterns.battle.teamFormation[location], { DoClick: true, HoldDurationMs: 750, PredicatePattern: patterns.battle.teamFormation.preset });
 		const currentPreset = macroService.GetText(patterns.battle.teamFormation.preset.current);
 		if (currentPreset.replace(/ /g, '').match(presetRegex)) {
-			macroService.PollPattern(patterns.battle.teamFormation.preset.topLeft, { DoClick: true, ClickOffset: { X: -60, Y: 50 }, PredicatePattern: patterns.battle.teamFormation });
+			macroService.PollPattern(patterns.battle.teamFormation.preset.topLeft, { DoClick: true, ClickOffset: { X: -60, Y: 50 }, PredicatePattern: patterns.general.back });
 			continue;
 		}
 
@@ -43,7 +43,7 @@ function applyPreset(teamSlot) {
 
 		macroService.PollPoint(targetPreset.point, { DoClick: true, PredicatePattern: patterns.battle.teamFormation.preset.ok });
 		macroService.PollPattern(patterns.battle.teamFormation.preset.ok, { DoClick: true, ClickPattern: patterns.battle.teamFormation.preset.ok2, PredicatePattern: patterns.battle.teamFormation.preset.presetList });
-		macroService.PollPattern(patterns.battle.teamFormation.preset.topLeft, { DoClick: true, ClickOffset: { X: -60 }, PredicatePattern: patterns.battle.teamFormation });		
+		macroService.PollPattern(patterns.battle.teamFormation.preset.topLeft, { DoClick: true, ClickOffset: { X: -60 }, PredicatePattern: patterns.general.back });		
 	}
 
 	if (macroService.IsRunning) {
