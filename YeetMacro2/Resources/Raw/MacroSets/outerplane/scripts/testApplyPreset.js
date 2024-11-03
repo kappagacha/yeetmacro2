f9b3f0a9154ee3bce84127123f9fc8c0804f6817
adiamante <angelo.c.diamante@gmail.com>
2024-11-02 19:46:38 -0500

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/testApplyPreset.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/testApplyPreset.js
@@ -43,7 +43,7 @@ macroService.PollPattern(patterns.battle.teamFormation[location], { DoClick: tru
 macroService.PollPattern(patterns.battle.teamFormation.preset, { DoClick: true, PredicatePattern: patterns.battle.teamFormation.preset.presetList });
 const presetCornerResult = macroService.FindPattern(patterns.battle.teamFormation.preset.corner, { Limit: 10 });
 const presetNames = presetCornerResult.Points.filter(p => p).map(p => {
-	const presetNamePattern = macroService.ClonePattern(patterns.battle.teamFormation.preset.name, { X: p.X + 13, Y: p.Y + 10, OffsetCalcType: 'None', Path: `battle.teamFormation.preset.name_x${p.X}_y${p.Y}` });
+	const presetNamePattern = macroService.ClonePattern(patterns.battle.teamFormation.preset.name, { X: p.X + 13, Y: p.Y + 8, OffsetCalcType: 'None', Path: `battle.teamFormation.preset.name_x${p.X}_y${p.Y}` });
 	return {
 		point: { X: p.X, Y: p.Y },
 		name: macroService.GetText(presetNamePattern, preset)
