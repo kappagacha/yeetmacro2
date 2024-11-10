cd750d3358002d4200918ef40601596734385016
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2024-11-09 22:08:14 -0600

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doSpecialRequestsStage13.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doSpecialRequestsStage13.js
@@ -61,7 +61,7 @@ function sweepAllStage13() {
 	const stage13AllPattern = macroService.ClonePattern(patterns.challenge.specialRequest.stage._13, { Height: 600, Path: 'patterns.challenge.specialRequest.stage._13_all' });
 	let stage13AllResult = macroService.FindPattern(stage13AllPattern);
 	while (stage13AllResult.IsSuccess) {
-		const currentStamina = parseInt(macroService.GetText(patterns.challenge.specialRequest.currentStamina));
+		const currentStamina = parseInt(macroService.GetText(patterns.challenge.specialRequest.currentStamina).split('/')[0]);
 		const maxRuns = parseInt(currentStamina / 16);
 
 		logger.info(`doSpecialRequestsStage13: currentStamina=${currentStamina}, maxRuns=${maxRuns}`);
