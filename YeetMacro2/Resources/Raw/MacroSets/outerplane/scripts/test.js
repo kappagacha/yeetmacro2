ec4626dec97f5a2332fab0a26c8906795a8d4e7a
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-03-29 17:05:45 -0500

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/test.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/test.js
@@ -1,4 +1,4 @@
 const popupPatterns = [patterns.lobby.expedition, patterns.general.tapEmptySpace, settings.goToLobby.userClickPattern.Value, patterns.general.exitCheckIn, patterns.general.startMessageClose];
 const loopPatterns = [patterns.lobby.level, patterns.titles.guildHallOfHonor, patterns.titles.guild, ...popupPatterns];
 
-macroService.PollPattern(loopPatterns, { DoClick: true, PredicatePattern: patterns.guild.hallOfHonor.recieveMessage });
+macroService.PollPattern(loopPatterns, { ClickPattern: patterns.friends.ok });
