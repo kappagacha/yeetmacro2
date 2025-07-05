7ed6f6be0bcdb3c9b58798dc79c2de87e13962d7
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-07-05 12:50:04 -0500

    add copy to clipboard with MessageView

--- YeetMacro2/Platforms/Android/ViewModels/MessageViewModel.cs
+++ YeetMacro2/Platforms/Android/ViewModels/MessageViewModel.cs
@@ -1,8 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using YeetMacro2.Services;
 
 namespace YeetMacro2.Platforms.Android.ViewModels;
 public partial class MessageViewModel : ObservableObject
 {
+    IToastService _toastService;
+    public MessageViewModel(IToastService toastSevice)
+    {
+        _toastService = toastSevice;
+    }
+
     [ObservableProperty]
     string _message;
+
+    [RelayCommand]
+    public async Task CopyMessgeToClipboard(String message)
+    {
+        await Clipboard.Default.SetTextAsync(message);
+        _toastService.Show($"Copied message to clipboard: {message}");
+    }
 }
