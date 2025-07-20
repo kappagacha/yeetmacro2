67c8d3c8f8c119ea1a717bbb1bcecf1144850fba
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-07-20 16:05:31 -0500

    Fix TestSwipe

--- YeetMacro2/ViewModels/NodeViewModels/PatternNodeManagerViewModel.cs
+++ YeetMacro2/ViewModels/NodeViewModels/PatternNodeManagerViewModel.cs
@@ -443,10 +443,11 @@ public partial class PatternNodeManagerViewModel : NodeManagerViewModel<PatternN
         MainThread.BeginInvokeOnMainThread(async () =>
         {
             _screenService.DrawClear();
-            _screenService.DrawRectangle(pattern.Bounds.Offset(pattern.Offset));
             _screenService.DoSwipe(pattern);     //one to change focus
             await Task.Delay(300);
             _screenService.DoSwipe(pattern);     //one to swipe
+            await Task.Delay(300);
+            _screenService.DebugRectangle(pattern.Bounds.Offset(pattern.Offset));
         });
     }
 }
