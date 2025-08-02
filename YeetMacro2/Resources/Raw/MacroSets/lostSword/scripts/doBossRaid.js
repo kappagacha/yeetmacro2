8fc89cea755bb4b0e8d964f1dbd689b9dec1e833
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-08-01 22:07:36 -0500

    update lostSword

--- YeetMacro2/Resources/Raw/MacroSets/lostSword/scripts/doBossRaid.js
+++ YeetMacro2/Resources/Raw/MacroSets/lostSword/scripts/doBossRaid.js
@@ -1,6 +1,6 @@
-const loopPatterns = [patterns.lobby, patterns.battle.title, patterns.bossRaid, patterns.stonehenge.select];
+const loopPatterns = [patterns.lobby, patterns.battle.title, patterns.bossRaid];
 const daily = dailyManager.GetCurrentDaily();
-if (daily.doStonehenge.done.IsChecked) {
+if (daily.doBossRaid.done.IsChecked) {
 	return "Script already completed. Uncheck done to override daily flag.";
 }
 
@@ -8,23 +8,33 @@ while (macroService.IsRunning) {
 	const loopResult = macroService.PollPattern(loopPatterns);
 	switch (loopResult.Path) {
 		case 'lobby':
-			logger.info('doStonehenge: click battle');
+			logger.info('doBossRaid: click battle');
 			macroService.ClickPattern(patterns.battle);
 			break;
 		case 'battle.title':
-			logger.info('doColosseum: click stonehenge');
-			macroService.PollPattern(patterns.battle.dungeon, { DoClick: true, PredicatePattern: patterns.battle.dungeon.selected });
-			macroService.PollPattern(patterns.stonehenge, { DoClick: true, PredicatePattern: patterns.stonehenge.select });
+			logger.info('doColosseum: click boss raid');
+			macroService.ClickPattern(patterns.battle.bossRaid);
 			break;
-		case 'stonehenge.select':
-			macroService.PollPattern(patterns.stonehenge.select, { DoClick: true, PredicatePattern: patterns.stonehenge.start });
-			macroService.PollPattern(patterns.stonehenge.summonBossAutoUse, { DoClick: true, PredicatePattern: patterns.stonehenge.summonBossAutoUse.checked });
-			sleep(5_000);
-			macroService.PollPattern(patterns.stonehenge.summonBossAutoUse.checked, { DoClick: true, PredicatePattern: patterns.stonehenge.summonBossAutoUse });
-			macroService.PollPattern(patterns.menu, { DoClick: true, PredicatePattern: patterns.menu.close });
-			macroService.PollPattern(patterns.menu.return, { DoClick: true, PredicatePattern: patterns.lobby });
+		case 'bossRaid':
+			if (settings.doBossRaid.enterOnce.Value) {
+				macroService.PollPattern(patterns.battle.enter, { DoClick: true, PredicatePattern: patterns.general.tapTheScreen });
+				macroService.PollPattern(patterns.general.tapTheScreen, { DoClick: true, PredicatePattern: patterns.bossRaid });
+				if (macroService.IsRunning) daily.doBossRaid.entered.IsChecked = true;
+			}
 
-			if (macroService.IsRunning) daily.doStonehenge.done.IsChecked = true;
+			let sweepResult = macroService.PollPattern(patterns.battle.sweep, { DoClick: true, PredicatePattern: [patterns.battle.sweep.confirm, patterns.general.itemsAcquired], TimeoutMs: 3_000 });
+
+
+			while (sweepResult.IsSuccess) {
+				if (sweepResult.PredicatePath === 'battle.sweep.confirm') {
+					macroService.PollPattern(patterns.battle.sweep.dontAskAgain, { DoClick: true, PredicatePattern: patterns.battle.sweep.dontAskAgain.checked });
+					macroService.PollPattern(patterns.battle.sweep.confirm, { DoClick: true, PredicatePattern: patterns.general.itemsAcquired });
+					macroService.PollPattern(patterns.general.itemsAcquired, { DoClick: true, PredicatePattern: patterns.bossRaid });
+				}
+				macroService.PollPattern(patterns.battle.sweep, { DoClick: true, PredicatePattern: [patterns.battle.sweep.confirm, patterns.general.itemsAcquired], TimeoutMs: 3_000 });
+			}
+
+			if (macroService.IsRunning) daily.doBossRaid.done.IsChecked = true;
 
 			return;
 	}
