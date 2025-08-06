bf5ea7c0d9596f40ec7a85a65293971f2dfd3362
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-08-05 22:22:30 -0500

    update lostSword

---
+++ YeetMacro2/Resources/Raw/MacroSets/lostSword/scripts/doUpkeep.js
@@ -0,0 +1,15 @@
+﻿// @isFavorite
+// @position=-1
+
+goToLobby();
+
+if (settings.doUpkeep.claimTreasureExploration.Value) {
+    claimTreasureExploration();
+    goToLobby();
+}
+
+if (settings.doUpkeep.doColoseum.Value) {
+    doColoseum();
+    goToLobby();
+}
+
