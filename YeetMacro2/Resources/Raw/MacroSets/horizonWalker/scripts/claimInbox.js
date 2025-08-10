3354ed3ac413ac580b6ca95dab0edccb43993441
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-08-10 16:08:09 -0500

    update horizonWalker

---
+++ YeetMacro2/Resources/Raw/MacroSets/horizonWalker/scripts/claimInbox.js
@@ -0,0 +1,27 @@
+﻿// claim inbox
+const loopPatterns = [patterns.phone.battery, patterns.phone.inbox.title];
+const daily = dailyManager.GetCurrentDaily();
+
+if (daily.claimInbox.done.IsChecked) {
+	return "Script already completed. Uncheck done to override daily flag.";
+}
+
+while (macroService.IsRunning) {
+	const loopResult = macroService.PollPattern(loopPatterns);
+	switch (loopResult.Path) {
+		case 'phone.battery':
+			logger.info('claimInbox: click inbox');
+			macroService.ClickPattern(patterns.phone.inbox);
+			break;
+		case 'phone.inbox.title':
+			logger.info('claimInbox: redeem all');
+			//sleep(1_000);
+			//macroService.PollPattern(patterns.checklist.redeemAll, { DoClick: true, PredicatePattern: patterns.general.touchTheScreen });
+			//sleep(1_000);
+			//macroService.PollPattern(patterns.general.touchTheScreen, { DoClick: true, PredicatePattern: patterns.checklist.title });
+
+			macroService.IsRunning && (daily.claimInbox.done.IsChecked = true);
+			return;
+	}
+	sleep(1_000);
+}
