fd8bb1ca58a4f6f56d315edf76b75b7a08329010
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-09-07 15:24:06 -0500

    update eversoul

---
+++ YeetMacro2/Resources/Raw/MacroSets/eversoul/scripts/test.js
@@ -0,0 +1,13 @@
+﻿const targetSoul = macroService.ClonePattern(settings.doManageTown.bond.targetSoul.Value, {
+	X: 275,
+	Y: 80,
+	Width: 1005,
+	Height: 900,
+	Path: 'settings.doManageTown.bond.targetSoul',
+	OffsetType: 'DockLeft',
+	BoundsCalcType: 'FillWidth'
+});
+
+//return targetSoul;
+macroService.PollPattern(targetSoul, { PredicatePattern: patterns.lobby.level });
+
