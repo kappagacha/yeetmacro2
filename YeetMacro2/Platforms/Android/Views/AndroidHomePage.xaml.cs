7d99576fa845b0ae0291fc95fe4f211febd0aa1e
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-09-07 19:31:14 -0500

    update AndroidHomePage

--- YeetMacro2/Platforms/Android/Views/AndroidHomePage.xaml.cs
+++ YeetMacro2/Platforms/Android/Views/AndroidHomePage.xaml.cs
@@ -1,3 +1,5 @@
+using YeetMacro2.Platforms.Android.ViewModels;
+
 namespace YeetMacro2.Platforms.Android.Views;
 
 public partial class AndroidHomePage : ContentPage
@@ -5,5 +7,14 @@ public partial class AndroidHomePage : ContentPage
 	public AndroidHomePage()
 	{
 		InitializeComponent();
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		if (BindingContext is AndriodHomeViewModel viewModel)
+		{
+			viewModel.AppearCommand.Execute(null);
+		}
 	}
 }
