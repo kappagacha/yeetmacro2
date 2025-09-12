eeff1106977edf924946511d8bbe7a4bf039249e
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-09-11 20:47:04 -0500

    update lostSword

---
+++ YeetMacro2/Resources/Raw/MacroSets/lostSword/scripts/test.js
@@ -0,0 +1,9 @@
+const coinResult = macroService.FindPattern(patterns.shop.coin, { Limit: 5 });
+let cointTypeResults = coinResult.Points.map(p => {
+	const cointTypePattern = macroService.ClonePattern(patterns.shop.coin.type, { CenterY: p.Y, OffsetCalcType: 'None', PathSuffix: `_${p.X}x_${p.Y}y` });
+	return {
+		point: { X: p.X, Y: p.Y },
+		text: macroService.FindText(cointTypePattern)
+	};
+});
+return cointTypeResults;
