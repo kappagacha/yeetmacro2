c040bb940abe4aca9c3ba8f257780d08930b0bf4
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2025-10-07 20:48:22 -0500

    update horizonWalker

--- YeetMacro2/Resources/Raw/MacroSets/horizonWalker/scripts/goToPhone.js
+++ YeetMacro2/Resources/Raw/MacroSets/horizonWalker/scripts/goToPhone.js
@@ -1,9 +1,8 @@
 ﻿// Go to lobby
 logger.info('goToPhone: start');
 
-const loopPatterns = [patterns.phone.battery, patterns.notice.close];
+const loopPatterns = [patterns.startScreen.settings, patterns.phone.battery, patterns.notice.close];
 const clickPatterns = [
-	patterns.startScreen.touchTheScreen,
 	patterns.phone,
 	patterns.general.back,
 	patterns.tribute.close,
@@ -13,6 +12,9 @@ const clickPatterns = [
 while (macroService.IsRunning) {
 	const loopResult = macroService.PollPattern(loopPatterns, { ClickPattern: clickPatterns });
 	switch (loopResult.Path) {
+		case 'startScreen.settings':
+			macroService.PollPattern(patterns.startScreen.settings, { DoClick: true, ClickOffset: { X: -200 }, InversePredicatePattern: patterns.startScreen.settings });
+			break;
 		case 'notice.close':
 			logger.info('goToPhone: close notice');
 			macroService.PollPattern(patterns.notice.dontRemindMeToday, { DoClick: true, PredicatePattern: patterns.notice.dontRemindMeToday.checked });
