3ee4db0acaa6a85a5d0328ba6a98994d8d25444b
adiamante <angelo.c.diamante@gmail.com>
2025-11-12 16:16:33 -0600

    update horizonWalker

--- YeetMacro2/Resources/Raw/MacroSets/horizonWalker/scripts/goToPhone.js
+++ YeetMacro2/Resources/Raw/MacroSets/horizonWalker/scripts/goToPhone.js
@@ -1,7 +1,7 @@
 ﻿// Go to lobby
 logger.info('goToPhone: start');
 
-const loopPatterns = [patterns.startScreen.settings, patterns.phone.battery, patterns.notice.close];
+const loopPatterns = [patterns.startScreen.settings, patterns.phone.battery, patterns.notice.close, patterns.notice.attendance.close];
 const clickPatterns = [
 	patterns.phone,
 	patterns.general.back,
@@ -19,6 +19,12 @@ while (macroService.IsRunning) {
 			logger.info('goToPhone: close notice');
 			macroService.PollPattern(patterns.notice.dontRemindMeToday, { DoClick: true, PredicatePattern: patterns.notice.dontRemindMeToday.checked });
 			macroService.PollPattern(patterns.notice.close, { DoClick: true, PredicatePattern: [patterns.phone, patterns.phone.battery] });
+			return;
+		case 'notice.attendance..close':
+			logger.info('goToPhone: close attendance notice');
+			macroService.PollPattern(patterns.notice.attendance.claimAll, { DoClick: true, PredicatePattern: patterns.general.touchTheScreen });
+			macroService.PollPattern(patterns.general.touchTheScreen, { DoClick: true, PredicatePattern: patterns.notice.attendance.close });
+			macroService.PollPattern(patterns.notice.attendance.close, { DoClick: true, PredicatePattern: [patterns.phone, patterns.phone.battery] });
 			return;
 		case 'phone.battery':
 			logger.info('goToPhone: done');
