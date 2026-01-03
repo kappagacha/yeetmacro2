f60b3df2c352573ca99dc389606e9cdcd6ffbe74
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2026-01-02 18:50:54 -0600

    update lostSword

--- YeetMacro2/Resources/Raw/MacroSets/lostSword/scripts/test.js
+++ YeetMacro2/Resources/Raw/MacroSets/lostSword/scripts/test.js
@@ -1,9 +1,22 @@
+const topLeft = macroService.GetTopLeft();
 const coinResult = macroService.FindPattern(patterns.shop.coin, { Limit: 5 });
 let cointTypeResults = coinResult.Points.map(p => {
-	const cointTypePattern = macroService.ClonePattern(patterns.shop.coin.type, { CenterY: p.Y, PathSuffix: `_${p.X}x_${p.Y}y` });
+	const coinTypeBounds = {
+		X: 112 + topLeft.X,
+		Y: p.Y - 19,
+		Height: 40.5,
+		Width: p.X - 35 - 112 - topLeft.X
+	};
+
+	//while (macroService.IsRunning) {
+	//	macroService.DebugRectangle(coinTypeBounds);
+	//	sleep(1_000);
+	//}
+
 	return {
 		point: { X: p.X, Y: p.Y },
-		text: macroService.FindText(cointTypePattern)
+		text: macroService.FindTextWithBounds(coinTypeBounds)
 	};
 });
+
 return cointTypeResults;
