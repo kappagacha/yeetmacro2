36c12e087f6d1134583fe9230db6ba0712ffd6bf
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2026-01-13 00:02:24 -0600

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/test.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/test.js
@@ -1,7 +1,24 @@
 // @tags=test
 
-const currentStamina = getCurrentStaminaValue();
-return { currentStamina };
+const arenaTicketCount = getArenaTicketCount();
+
+const ticketResult = macroService.PollPattern(patterns.arena.ticket);
+const slashPattern = macroService.ClonePattern(patterns.arena.ticket.slash, { X: ticketResult.Point.X + 60, Width: 100, Padding: 5, PathSuffix: `_x${ticketResult.Point.X}`, OffsetCalcType: 'None' });
+const slashResult = macroService.PollPattern(slashPattern);
+const valueBounds = {
+	X: ticketResult.Point.X + 60,
+	Y: slashResult.Point.Y - 9,
+	Height: slashPattern.Height + 5,
+	Width: slashResult.Point.X - ticketResult.Point.X - 70
+};
+while (macroService.IsRunning) {
+	macroService.DebugRectangle(valueBounds);
+	sleep(1_000);
+}
+
+arenaTicketCount = macroService.FindTextWithBounds(valueBounds);
+
+return { arenaTicketCount };
 
 //const unitTitleAndName = macroService.FindText(patterns.battle.teamFormation.unitTitleAndName);
 //return unitTitleAndName;
