8e80fe705da9ecd418e2fa9de961b0f9f7809471
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2026-01-19 17:59:53 -0600

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doAdventureLicense.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doAdventureLicense.js
@@ -21,28 +21,36 @@ while (macroService.IsRunning) {
 			logger.info('doAdventureLicense: do weekly conquest');
 			macroService.PollPattern(patterns.adventure.adventureLicense.weeklyConquest, { DoClick: true, PredicatePattern: patterns.adventure.adventureLicense.weeklyConquest.selected });
 
-			const wantedResults = macroService.FindPattern(patterns.adventure.adventureLicense.weeklyConquest.wanted, { Limit: 4 });
-			let targetWanted;
-			for (const p of wantedResults.Points) {
-				const completedPattern = macroService.ClonePattern(patterns.adventure.adventureLicense.weeklyConquest.completed,
-					{ CenterX: p.X - 10, CenterY: p.Y - 130, Height: 120, Width: 270, PathSuffix: `_${p.X}x` });
-				if (!macroService.FindPattern(completedPattern).IsSuccess) {
-					targetWanted = p;
-					break;
-				}
+			macroService.PollPattern(patterns.adventure.adventureLicense.weeklyConquest.wanted);
+			let incompleteWanted = findIncompleteWanted();
+
+			if (!incompleteWanted) {
+				macroService.SwipePattern(patterns.general.swipeRight);
+				sleep(2_500);
+				incompleteWanted = findIncompleteWanted();
 			}
 
-			if (!targetWanted) {
+			if (!incompleteWanted) {
 				throw Error('Could not find wanted that is not complete');
 			}
-			macroService.PollPoint(targetWanted, { DoClick: true, PredicatePattern: patterns.adventure.adventureLicense.weeklyConquest.heroDeployment });
+			macroService.PollPoint(incompleteWanted, { DoClick: true, PredicatePattern: patterns.adventure.adventureLicense.weeklyConquest.heroDeployment });
 			macroService.PollPattern(patterns.adventure.adventureLicense.weeklyConquest.heroDeployment, { DoClick: true, PredicatePattern: patterns.battle.enter });
 			selectTeam2();
 			macroService.PollPattern(patterns.general.back, { DoClick: true, PrimaryClickInversePredicatePattern: patterns.battle.enter, PredicatePattern: patterns.battle.enter });
 			macroService.PollPattern(patterns.battle.enter, { DoClick: true, PredicatePattern: patterns.adventure.adventureLicense.weeklyConquest.next });
 			macroService.PollPattern(patterns.adventure.adventureLicense.weeklyConquest.next, { DoClick: true, PredicatePattern: patterns.adventure.adventureLicense.weeklyConquest.exit });
 			macroService.PollPattern(patterns.adventure.adventureLicense.weeklyConquest.exit, { DoClick: true, PredicatePattern: patterns.adventure.adventureLicense.weeklyConquest.selected });
-			return;
 	}
 	sleep(1_000);
+}
+
+function findIncompleteWanted() {
+	const wantedResults = macroService.FindPattern(patterns.adventure.adventureLicense.weeklyConquest.wanted, { Limit: 4 });
+	for (const p of wantedResults.Points.sort((a, b) => a.X - b.X)) {
+		const completedPattern = macroService.ClonePattern(patterns.adventure.adventureLicense.weeklyConquest.completed,
+			{ CenterX: p.X - 10, CenterY: p.Y - 130, Height: 120, Width: 270, PathSuffix: `_${p.X}x` });
+		if (!macroService.FindPattern(completedPattern).IsSuccess) {
+			return p;
+		}
+	}
 }
