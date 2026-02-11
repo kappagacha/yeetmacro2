ec082d650fe7949a402ecb0038d580943408ebed
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2026-02-11 13:30:42 -0600

    update outerplane

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doGuildRaid.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/doGuildRaid.js
@@ -32,38 +32,29 @@ while (macroService.IsRunning) {
 			macroService.ClickPattern(patterns.guild.raid.move);
 			break;
 		case 'titles.guildRaid':
-			if (!daily.doGuildRaid.battle1.IsChecked) {
-				macroService.PollPattern(patterns.guild.raid.selectTeam, { DoClick: true, ClickPattern: [patterns.general.tapEmptySpace, patterns.guild.raid.ok], PredicatePattern: patterns.battle.enter });
+			const selectTeamResult = macroService.PollPattern([patterns.guild.raid.selectTeam, patterns.guild.raid.selectTeam1, patterns.guild.raid.selectTeam2]);
+			const isPhase1 = selectTeamResult.Path === 'guild.raid.selectTeam1' || selectTeamResult.Path === 'guild.raid.selectTeam2';
+
+			for (const battleNum of [1, 2]) {
+				const battleKey = `battle${battleNum}`;
+				const recordKey = isPhase1 ? 'record1' : `record${battleNum}`;
+				const selectKey = isPhase1 ? `selectTeam${battleNum}` : 'selectTeam';
+				if (daily.doGuildRaid[battleKey].IsChecked) continue;
+
+				logger.info(`doGuildRaid: battle ${battleNum}`);
+				macroService.PollPattern(patterns.guild.raid[selectKey], { DoClick: true, ClickPattern: [patterns.general.tapEmptySpace, patterns.guild.raid.ok], PredicatePattern: patterns.battle.enter });
 				selectTeam(teamSlot);
 				macroService.PollPattern(patterns.guild.raid.battleRecord, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord.restoreTeam });
-				macroService.PollPattern(patterns.guild.raid.battleRecord.record1, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord.record1.selected });
+				macroService.PollPattern(patterns.guild.raid.battleRecord[recordKey], { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord[recordKey].selected });
 				macroService.PollPattern(patterns.guild.raid.battleRecord.restoreTeam, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord.restoreTeam.ok });
 				macroService.PollPattern(patterns.guild.raid.battleRecord.restoreTeam.ok, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord });
 				macroService.PollPattern(patterns.battle.teamFormation, { DoClick: true, PredicatePattern: patterns.battle.teamFormation.selected });
 				setChainOrder();
-				macroService.PollPattern(patterns.battle.enter, { DoClick: true, PredicatePattern: patterns.guild.raid.enterBattle });
-				macroService.PollPattern(patterns.guild.raid.enterBattle, { DoClick: true, PredicatePattern: patterns.guild.raid.exitBattle });
+				macroService.PollPattern(patterns.battle.enter, { DoClick: true, PredicatePattern: patterns.guild.raid.exitBattle });
 				macroService.PollPattern(patterns.guild.raid.exitBattle, { DoClick: true, ClickPattern: [patterns.general.tapEmptySpace, patterns.guild.raid.ok], PredicatePattern: patterns.titles.guildRaid });
-				macroService.PollPattern(patterns.guild.raid.selectTeam, { DoClick: true, PredicatePattern: patterns.battle.enter });
-				
-				if (macroService.IsRunning) daily.doGuildRaid.battle1.IsChecked = true;
-			}
+				//macroService.PollPattern(patterns.guild.raid[selectKey], { DoClick: true, PredicatePattern: patterns.battle.enter });
 
-			if (!daily.doGuildRaid.battle2.IsChecked) {
-				macroService.PollPattern(patterns.guild.raid.selectTeam, { DoClick: true, ClickPattern: [patterns.general.tapEmptySpace, patterns.guild.raid.ok], PredicatePattern: patterns.battle.enter });
-				selectTeam(teamSlot);
-				macroService.PollPattern(patterns.guild.raid.battleRecord, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord.restoreTeam });
-				macroService.PollPattern(patterns.guild.raid.battleRecord.record2, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord.record2.selected });
-				macroService.PollPattern(patterns.guild.raid.battleRecord.restoreTeam, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord.restoreTeam.ok });
-				macroService.PollPattern(patterns.guild.raid.battleRecord.restoreTeam.ok, { DoClick: true, PredicatePattern: patterns.guild.raid.battleRecord });
-				macroService.PollPattern(patterns.battle.teamFormation, { DoClick: true, PredicatePattern: patterns.battle.teamFormation.selected });
-				setChainOrder();
-				macroService.PollPattern(patterns.battle.enter, { DoClick: true, PredicatePattern: patterns.guild.raid.enterBattle });
-				macroService.PollPattern(patterns.guild.raid.enterBattle, { DoClick: true, PredicatePattern: patterns.guild.raid.exitBattle });
-				macroService.PollPattern(patterns.guild.raid.exitBattle, { DoClick: true, ClickPattern: [patterns.general.tapEmptySpace, patterns.guild.raid.ok], PredicatePattern: patterns.titles.guildRaid });
-				macroService.PollPattern(patterns.guild.raid.selectTeam, { DoClick: true, PredicatePattern: patterns.battle.enter });
-
-				if (macroService.IsRunning) daily.doGuildRaid.battle2.IsChecked = true;
+				if (macroService.IsRunning) daily.doGuildRaid[battleKey].IsChecked = true;
 			}
 
 			if (macroService.IsRunning) daily.doGuildRaid.done.IsChecked = true;
