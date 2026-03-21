34dbc6198084797461e672a549851dd4c6383308
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2026-03-20 21:18:32 -0500

    Update sweepEventStoryHard.js

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/sweepEventStoryHard.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/sweepEventStoryHard.js
@@ -49,7 +49,7 @@ while (macroService.IsRunning) {
 			handleRewards();
 
 			if (done) {
-				settings.doDailies.sweepEventStoryHard2.Value = false;
+				settings.doDailies.sweepEventStoryHard.Value = false;
 				return;
 			}
 
