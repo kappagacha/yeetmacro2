720e3a919d07acf93ec50dc820f10e874e7ca18e
Angelo Diamante <72841758+adiamante@users.noreply.github.com>
2026-06-27 20:45:36 -0500

    Update test.js

--- YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/test.js
+++ YeetMacro2/Resources/Raw/MacroSets/outerplane/scripts/test.js
@@ -170,7 +170,11 @@ if (settings.test.type.Value === 'arenaTicketCount') {
 	const numDaysDetected = {}
 	for (const p of daysResult.Points.sort((a, b) => a.Y - b.Y)) {
 		const dayBounds = { X: p.X - 27, Y: p.Y - 15, Width: 30, Height: 30 };
-		const numDays = parseInt(macroService.FindTextWithBounds(dayBounds, "1234567890").slice(0, -1));
+		let text = macroService.FindTextWithBounds(dayBounds, "1234567890");
+		if (text.length > 1) {
+			text = text.slice(0, -1)
+		}
+		const numDays = parseInt(text);
 		numDaysDetected[p.Y] = numDays
 	}
 	return numDaysDetected;
